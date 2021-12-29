469f4cfa4ee8f3ce25aaf7e8458faef50113a512
besumero628 <besumero628@gmail.com>
2021-12-29 16:03:24 +0900

    tsファイルの更新

--- sample.ts
+++ sample.ts
@@ -1,1 +1,4 @@
-console.log("Welcome to Typescript!!")
+window.addEventListener('load', (event)=> {
+  let p = document.querySelector('#target')
+  p.textContent = "This is message by TypeScript"
+})
